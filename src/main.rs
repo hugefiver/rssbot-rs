6c684c5afced9e51739e019bd16c76d542bd9542
Hugefiver <i@iruri.moe>
2023-07-04 03:29:07 +0000

    replace structopt with clap

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,9 @@ use std::process;
 use std::sync::Arc;
 
 use anyhow::Context;
+use clap::Parser;
 use hyper_proxy::{Intercept, Proxy};
 use once_cell::sync::OnceCell;
-use structopt::StructOpt;
 
 use tokio::{self, sync::Mutex};
 
@@ -33,8 +33,8 @@ use crate::data::Database;
 static BOT_NAME: OnceCell<String> = OnceCell::new();
 static BOT_ID: OnceCell<tbot::types::user::Id> = OnceCell::new();
 
-#[derive(Debug, StructOpt)]
-#[structopt(
+#[derive(Debug, clap::Parser)]
+#[command(
     about = "A simple Telegram RSS bot.",
     after_help = "NOTE: You can get <user id> using bots like @userinfobot @getidsbot"
 )]
@@ -42,38 +42,38 @@ pub struct Opt {
     /// Telegram bot token
     token: String,
     /// Path to database
-    #[structopt(
-        short = "d",
+    #[arg(
+        short = 'd',
         long,
         value_name = "path",
         default_value = "./rssbot.json"
     )]
     database: PathBuf,
     /// Minimum fetch interval
-    #[structopt(
+    #[arg(
         long,
         value_name = "seconds",
         default_value = "300",
-        validator(check_interval)
+        value_parser(parse_check_interval)
     )]
     // default is 5 minutes
     min_interval: u32,
     /// Maximum fetch interval
-    #[structopt(
+    #[arg(
         long,
         value_name = "seconds",
         default_value = "43200",
-        validator(check_interval)
+        value_parser(parse_check_interval)
     )]
     // default is 12 hours
     max_interval: u32,
     /// Maximum feed size, 0 is unlimited
-    #[structopt(long, value_name = "bytes", default_value = "2097152")]
+    #[arg(long, value_name = "bytes", default_value = "2097152")]
     // default is 2MiB
     max_feed_size: u64,
     /// Private mode, only specified user can use this bot.
     /// This argument can be passed multiple times to allow multiple admins
-    #[structopt(
+    #[arg(
         long,
         value_name = "user id",
         number_of_values = 1,
@@ -81,19 +81,19 @@ pub struct Opt {
     )]
     admin: Vec<i64>,
     /// Make bot commands only accessible for group admins.
-    #[structopt(long)]
+    #[arg(long)]
     restricted: bool,
     /// DANGER: Insecure mode, accept invalid TLS certificates
-    #[structopt(long)]
+    #[arg(long)]
     insecure: bool,
 }
 
-fn check_interval(s: String) -> Result<(), String> {
+fn parse_check_interval(s: &str) -> Result<u32, String> {
     s.parse::<u32>().map_err(|e| e.to_string()).and_then(|r| {
         if r < 1 {
             Err("must >= 1".into())
         } else {
-            Ok(())
+            Ok(r)
         }
     })
 }
@@ -102,7 +102,7 @@ fn check_interval(s: String) -> Result<(), String> {
 async fn main() -> anyhow::Result<()> {
     enable_fail_fast();
 
-    let opt = Opt::from_args();
+    let opt = Opt::parse();
     let db = Arc::new(Mutex::new(Database::open(opt.database.clone())?));
     let bot = if let Some(proxy) = init_proxy() {
         tbot::bot::Builder::with_string_token(opt.token.clone())
